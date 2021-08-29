d6449c319ae0c9e1c417ce6dd3d29242955063cf
asun.gomez <agcastro7@gmail.com>
2021-08-29 15:11:33 +0200

    First empty view

--- frontend/src/components/empty/empty.tsx
+++ frontend/src/components/empty/empty.tsx
@@ -1,3 +1,45 @@
+import './empty.scss';
+
+import {
+  EuiButton,
+  EuiFlexGroup,
+  EuiFlexItem,
+  EuiLink,
+  EuiSpacer,
+  EuiText,
+} from '@elastic/eui';
 import React from 'react';
 
-export const Empty: React.FC<{}> = () => <>Empty</>;
+import { ReactComponent as Image } from '../../assets/images/tumbleweed.svg';
+
+export const Empty: React.FC<{}> = () => (
+  <EuiFlexGroup direction="column" alignItems="center" gutterSize="xs">
+    <EuiFlexItem grow={false}>
+      <EuiFlexGroup direction="row" justifyContent="center" responsive={false}>
+        <EuiFlexItem grow={false}>
+          <Image className="empty__image" />
+        </EuiFlexItem>
+      </EuiFlexGroup>
+    </EuiFlexItem>
+    <EuiFlexItem grow={false}>
+      <EuiText size="xs" color="subdued" textAlign="center">
+        Image by{' '}
+        <EuiLink href="https://www.freepik.com" target="_blank">
+          Freepik
+        </EuiLink>{' '}
+        from{' '}
+        <EuiLink href="https://www.flaticon.com/" target="_blank">
+          Flaticon
+        </EuiLink>
+      </EuiText>
+    </EuiFlexItem>
+    <EuiSpacer />
+    <EuiFlexItem grow={false}>
+      <EuiFlexGroup direction="row" justifyContent="center" responsive={false}>
+        <EuiFlexItem grow={false}>
+          <EuiButton fill>Create a goal</EuiButton>
+        </EuiFlexItem>
+      </EuiFlexGroup>
+    </EuiFlexItem>
+  </EuiFlexGroup>
+);
